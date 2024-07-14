cb19f4f18392d557b5986724c63b946385151b34
Saurabhvermaji <gaminggalaxy2022@gmail.com>
2024-07-14 13:23:16 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux'
 import './styles/index.css';
-import App from './App';
+import App from './App'
 import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
